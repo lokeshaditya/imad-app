c40c2ab00491903545b5aaeba95d51d75132b2b7
lokeshaditya <lokeshaditya.3.6.9@gmail.com>
2017-08-14 12:56:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article3', function (req, res){
 });
 
 app.get('/article2', function (req, res){
-    res.senfFile(path.join(__dirname, 'ui', 'article2.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
